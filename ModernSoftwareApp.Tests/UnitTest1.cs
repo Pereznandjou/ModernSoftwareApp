9de839eafa802cc8bab920c6b3d62f5b7d6b0b9e
Pereznandjou <leaderperez12@icloud.com>
2025-04-17 12:01:18 +0200

    feat: Init projet ModernSoftwareApp c#

---
+++ ModernSoftwareApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using ModernSoftwareApp; 
+
+namespace ModernSoftwareApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            int result = calc.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ ModernSoftwareApp/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ModernSoftwareApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+    }
+}
